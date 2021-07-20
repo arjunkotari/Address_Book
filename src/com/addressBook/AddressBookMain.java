20afc267c9786684877335c721fee90a6022a5a5
arjunkotari <arjun.kotari123@gmail.com>
2021-07-20 22:22:38 +0530

    Added:Contact details

--- src/com/addressBook/AddressBookMain.java
+++ src/com/addressBook/AddressBookMain.java
@@ -3,7 +3,22 @@ package com.addressBook;
 import java.util.ArrayList;
 
 public class AddressBookMain {
+    /*
+    adding contact details
+     */
     public static void main(String[] args) {
-       System.out.println("Welcome to Address Book program ");
+        //global variables
+        String firstName = "Arjun";
+        String lastName = "Sai";
+        String city = "ABC";
+        String state = "XYZ" ;
+        String zip = "001";
+        String phoneNumber = "007";
+        String email = "arjun@gmail.com";
+        //calling constructor through object creation.
+        Contacts c1 = new Contacts(firstName, lastName, city, state, zip, phoneNumber, email);
+        ArrayList<Contacts> list = new ArrayList<Contacts>();
+        list.add(c1);
+        System.out.println(c1);
         }
 }
